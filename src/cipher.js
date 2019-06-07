e16519bd2fbb8d2c2c1f4ba9796905ce9126aca7
montezakarim <kmonteza@hotmail.com>
2019-06-07 01:35:15 -0500

    estilos

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,16 @@
 window.cipher = {
-  encode: (offset, string) => {
+  encode: (offset1, string1) => {
     /* Acá va tu código que cifra*/
-  },
+let MensajeCifrado='';
+for (lef i = 0; i < string1.length; i++) {
+  let position= (string1.toUpperCase().charCodeAt(1)-65+ parseInt(offset1))%26+65;
+  let mensaje=String.fromCharCode(position);
+  MensajeCifrado+=mensaje;
+ }
+return MensajeCifrado
+ }
+
+
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
   }
