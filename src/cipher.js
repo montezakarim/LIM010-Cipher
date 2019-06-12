e65effa4d8a2529a3205bbceb3127f2b1f9872c1
montezakarim <kmonteza@hotmail.com>
2019-06-12 11:01:44 -0500

    leer tambien minúsculas, numeros, y caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,43 @@
 window.cipher = {
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
-let MensajeCifrado='';
-let position=0;
-for (let i = 0; i < string.length; i++) {
-  if (string.charCodeAt(i)===32) {
-    position=string.charCodeAt(i);
-  }
-  else {
-    position= (string.charCodeAt(i)-65+ parseInt(offset))%26+65;
-  }
-  let mensaje=String.fromCharCode(position);
-  MensajeCifrado+=mensaje;
-}
-return MensajeCifrado;
-},
+    let MensajeCifrado='';
+    let position=0;
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i)===32) {
+      position=string.charCodeAt(i);
+      }
+      else if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+      position= (string.charCodeAt(i)-65+ parseInt(offset))%26+65;
+      }
+      else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+      position= (string.charCodeAt(i)-97+ parseInt(offset))%26+97;
+      }
+      else if(string.charCodeAt(i)>=33 && string.charCodeAt(i)<=64){
+        position= (string.charCodeAt(i)-33+ parseInt(offset))%32+33;
+      } 
+      let mensaje=String.fromCharCode(position);
+      MensajeCifrado+=mensaje;
+    }
+    return MensajeCifrado;
+  },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
     let Mensajedescifrado='';
     let position1=0;
     for (let i = 0; i < string.length; i++) {
-      if (string.charCodeAt(i)===32) {
+       if (string.charCodeAt(i)===32) {
         position1=string.charCodeAt(i);
-      }
-      else {
+        }
+        else if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
         position1= 90-((90-string.charCodeAt(i)+ parseInt(offset))%26);
-      }
+        }
+        else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+        position1= 122-((122-string.charCodeAt(i)+ parseInt(offset))%26);
+        }
+        else if(string.charCodeAt(i)>=33 && string.charCodeAt(i)<=64){
+          position1= 64-((64-string.charCodeAt(i)+ parseInt(offset))%32);
+        } 
       let mensaje1=String.fromCharCode(position1);
       Mensajedescifrado+=mensaje1;
     }
