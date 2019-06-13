8de3b61fffd91a1364822cffec834717266e357b
montezakarim <kmonteza@hotmail.com>
2019-06-13 10:56:45 -0500

    Actualización Cipher, caracteres, numeros y else

--- src/cipher.js
+++ src/cipher.js
@@ -4,18 +4,18 @@ window.cipher = {
     let MensajeCifrado='';
     let position=0;
     for (let i = 0; i < string.length; i++) {
-      if (string.charCodeAt(i)===32) {
-      position=string.charCodeAt(i);
-      }
-      else if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
       position= (string.charCodeAt(i)-65+ parseInt(offset))%26+65;
       }
       else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
       position= (string.charCodeAt(i)-97+ parseInt(offset))%26+97;
       }
       else if(string.charCodeAt(i)>=33 && string.charCodeAt(i)<=64){
-        position= (string.charCodeAt(i)-33+ parseInt(offset))%32+33;
+      position= (string.charCodeAt(i)-33+ parseInt(offset))%32+33;
       } 
+      else{
+      position=string.charCodeAt(i);
+      }
       let mensaje=String.fromCharCode(position);
       MensajeCifrado+=mensaje;
     }
@@ -26,18 +26,18 @@ window.cipher = {
     let Mensajedescifrado='';
     let position1=0;
     for (let i = 0; i < string.length; i++) {
-       if (string.charCodeAt(i)===32) {
-        position1=string.charCodeAt(i);
-        }
-        else if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-        position1= 90-((90-string.charCodeAt(i)+ parseInt(offset))%26);
-        }
-        else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-        position1= 122-((122-string.charCodeAt(i)+ parseInt(offset))%26);
-        }
-        else if(string.charCodeAt(i)>=33 && string.charCodeAt(i)<=64){
-          position1= 64-((64-string.charCodeAt(i)+ parseInt(offset))%32);
-        } 
+      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+      position1= 90-((90-string.charCodeAt(i)+ parseInt(offset))%26);
+      }
+      else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+      position1= 122-((122-string.charCodeAt(i)+ parseInt(offset))%26);
+      }
+      else if(string.charCodeAt(i)>=33 && string.charCodeAt(i)<=64){
+      position1= 64-((64-string.charCodeAt(i)+ parseInt(offset))%32);
+      } 
+      else{
+      position1=string.charCodeAt(i);
+      }
       let mensaje1=String.fromCharCode(position1);
       Mensajedescifrado+=mensaje1;
     }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -9,17 +9,36 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33',() =>{
+      assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33',() =>{
+      assert.equal(cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg");
+    });
+    it('debería retornar "* ;<=>? 8 6" para "@ 12345 . ," con offset 10',() =>{
+      assert.equal(cipher.encode(10,"@ 12345 . ,"),"* ;<=>? 8 6");
+    });
+    it('debería retornar " " para " " con offset 2',() =>{
+      assert.equal(cipher.encode(2," ")," ");
+    });
   });
 
   describe('cipher.decode', () => {
-
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',() =>{
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33',() =>{
+      assert.equal(cipher.decode(33,"hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
+    });
+    it('debería retornar "@ 12345 . ," para "* ;<=>? 8 6" con offset 10',() =>{
+      assert.equal(cipher.decode(10,"* ;<=>? 8 6"),"@ 12345 . ,");
+    });
+    it('debería retornar " " para " " con offset 2',() =>{
+      assert.equal(cipher.decode(2," ")," ");
+    });
   });
 
 });
