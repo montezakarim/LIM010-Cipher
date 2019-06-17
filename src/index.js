b10b8ce01bb91c1836a2c877001ce1e07b3f341a
montezakarim <kmonteza@hotmail.com>
2019-06-17 12:26:14 -0500

    estilos, variables en inglés, footer, classlist

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
 /*declarar variables classList*/
-const nombrecontraseña=document.getElementById('nombrecontraseña');
-const cifrardescifrar=document.getElementById('cifrardescifrar');
-const cifrado=document.getElementById('cifrado');
-const descifrado=document.getElementById('descifrado');
-const mensajecifrado=document.getElementById('mensajecifrado');
-const mensajedescifrado=document.getElementById('Mensajedescifrado')
+const namePassword=document.getElementById('nombrecontraseña');
+const ciphers=document.getElementById('cifrardescifrar');
+const encode1=document.getElementById('cifrado');
+const decode1=document.getElementById('descifrado');
+const messageEncode=document.getElementById('mensajecifrado');
+const messageDecode=document.getElementById('Mensajedescifrado')
 /* Validando Contraseña */
-const btningresar=document.getElementById('ingresar');
-const contraseña= document.getElementById('contraseña');
-const incorrecta=document.getElementById('incorrecta')
-let contador=0;
-btningresar.addEventListener('click', () => {
-  if (contraseña.value==='LABORATORIA') {
-  const nombreusuario=document.getElementById('nombre').value;
-  document.getElementById('usuario').innerHTML='Hola '+ nombreusuario;
-  nombrecontraseña.classList.add('hide');
-  cifrardescifrar.classList.remove('hide');
+const btnEnter=document.getElementById('ingresar');
+const password= document.getElementById('contraseña');
+const incorrect=document.getElementById('incorrecta')
+let counter=0;
+btnEnter.addEventListener('click', () => {
+  if (password.value==='LABORATORIA') {
+  const nameUser=document.getElementById('nombre').value;
+  document.getElementById('usuario').innerHTML='Hola '+ nameUser;
+  namePassword.classList.add('hide');
+  ciphers.classList.remove('hide');
   }
-  if (contraseña.value!=='LABORATORIA') {
-    if (contraseña.value==='') {
-      incorrecta.innerHTML='Ingrese Contraseña';
+  if (password.value!=='LABORATORIA') {
+    if (password.value==='') {
+      incorrect.innerHTML='Ingrese Contraseña';
   }
   else {
-      contador=contador+1;
-      if (contador<3) {
-        incorrecta.innerHTML='Contraseña Incorrecta, intenta otra Vez';
+      counter=counter+1;
+      if (counter<3) {
+        incorrect.innerHTML='Contraseña Incorrecta, intenta otra Vez';
       }
       else {
-        incorrecta.innerHTML='Ya utilizaste todos tus intentos, en este momento no podrás ingresar';
-        btningresar.disabled=true;
+        incorrect.innerHTML='Ya utilizaste todos tus intentos, en este momento no podrás ingresar';
+        btnEnter.disabled=true;
     }
   }
   }
   document.getElementById('nombre').value='';
-  document.getElementById('contraseña').value='';
+  password.value='';
 });
 /* acción cifrar */
 const btncifrar=document.getElementById('cifrar');
 btncifrar.addEventListener('click', () => {
-  cifrardescifrar.classList.add('hide');
-  cifrado.classList.remove('hide');
+  ciphers.classList.add('hide');
+  encode1.classList.remove('hide');
   document.getElementById('text1').value='';
   document.getElementById('offset1').value='';
 });
 /* acción descifrar */
 const btndescifrar=document.getElementById('descifrar');
 btndescifrar.addEventListener('click', () => {
-  cifrardescifrar.classList.add('hide');
-  descifrado.classList.remove('hide');
+  ciphers.classList.add('hide');
+  decode1.classList.remove('hide');
   document.getElementById('text2').value='';
   document.getElementById('offset2').value='';
 });
@@ -59,8 +59,8 @@ btnoffsetcifrar.addEventListener('click', () => {
   const encriptado=document.getElementById('encriptado');
   let mensaje1=cipher.encode(offset1.value, string1.value);
   encriptado.innerHTML=(mensaje1);
-  cifrado.classList.add('hide');
-  mensajecifrado.classList.remove('hide');
+  encode1.classList.add('hide');
+  messageEncode.classList.remove('hide');
 });
 /* Ingresar offset para descifrar*/
 const btnoffsetdescifrar=document.getElementById('aceptardescifrar')
@@ -70,38 +70,36 @@ btnoffsetdescifrar.addEventListener('click',()=>{
   const mjdescifrado=document.getElementById('mjdescifrado');
   let mensaje2=cipher.decode(offset2.value, string2.value);
   mjdescifrado.innerHTML=(mensaje2)
-  descifrado.classList.add('hide');
-  mensajedescifrado.classList.remove('hide');
+  decode1.classList.add('hide');
+  messageDecode.classList.remove('hide');
 });
 /*finalizar y regresar al inicio*/
 const volver=document.getElementById('volver');
 volver.addEventListener('click', () => {
-  mensajecifrado.classList.add('hide');
-  cifrardescifrar.classList.remove('hide');
+  messageEncode.classList.add('hide');
+  ciphers.classList.remove('hide');
 });
 /*finalizar y regresar al inicio*/
 const volver1=document.getElementById('volver1');
 volver1.addEventListener('click', () => {
-  mensajedescifrado.classList.add('hide');
-  cifrardescifrar.classList.remove('hide');
+  messageDecode.classList.add('hide');
+  ciphers.classList.remove('hide');
 });
 /*finalizar y regresar al inicio*/
 const finalizar=document.getElementById('fin');
 finalizar.addEventListener('click', () => {
-  mensajecifrado.classList.add('hide');
-  nombrecontraseña.classList.remove('hide');
+  messageEncode.classList.add('hide');
+  namePassword.classList.remove('hide');
   document.getElementById('nombre').value='';
-  document.getElementById('contraseña').value='';
-  document.getElementById('incorrecta').innerHTML='';
-  incorrecta.innerHTML='';
+  password.value='';
+  incorrect.innerHTML='';
 });
 /*finalizar y regresar al inicio*/
 const finalizar1=document.getElementById('fin1');
 finalizar1.addEventListener('click', () => {
-mensajedescifrado.classList.add('hide');
-nombrecontraseña.classList.remove('hide')
+messageDecode.classList.add('hide');
+namePassword.classList.remove('hide')
   document.getElementById('nombre').value='';
-  document.getElementById('contraseña').value='';
-  document.getElementById('incorrecta').innerHTML="";
-  incorrecta.innerHTML='';
+  password.value='';
+  incorrect.innerHTML='';
 });
